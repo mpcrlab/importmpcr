f59aee67d98b5ca32980fedc5271e1387106cb47
William E Hahn <williamedwardhahn@gmail.com>
2019-02-23 17:04:57 -0500

    Update mpcr.py

--- mpcr.py
+++ mpcr.py
@@ -1,4 +1,27 @@
+import numpy as np
+from google.colab import drive
+import os
+import random
+from skimage.util.shape import view_as_windows, view_as_blocks
 import matplotlib.pyplot as plt
+from astropy.io import fits
+from os import listdir
+from os.path import join
+from PIL import Image
+from __future__ import print_function
+import argparse
+from math import log10
+import torch
+import torch.utils.data as data
+import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+import torch.backends.cudnn as cudnn
+from torch.nn import init
+import functools
+from torch.optim import lr_scheduler
+
 
 def plot(x):
   fig, ax = plt.subplots()
@@ -6,3 +29,94 @@ def plot(x):
   ax.axis('off')
   fig.set_size_inches(18, 10)
   plt.show()
+  
+  
+def scale(im):
+  
+  min_val = np.min(im.ravel())
+  max_val = np.max(im.ravel())
+  out = (im.astype('float') - min_val) / (max_val - min_val)
+    
+  return out
+  
+  
+def image_prep(X):
+
+  X = X - np.mean(X)
+  #     X = X / np.std(X)
+
+  return X
+
+
+def is_image_file(filename):
+    return any(filename.endswith(extension) for extension in [".png", ".jpg", ".jpeg"])
+
+
+def load_img(filepath):
+    img = Image.open(filepath).convert('RGB')
+    img = img.resize((256, 256), Image.BICUBIC)
+    return img
+
+
+def save_img(image_tensor, filename):
+    image_numpy = image_tensor.float().numpy()
+    image_numpy = (np.transpose(image_numpy, (1, 2, 0)) + 1) / 2.0 * 255.0
+    image_numpy = image_numpy.clip(0, 255)
+    image_numpy = image_numpy.astype(np.uint8)
+    image_pil = Image.fromarray(image_numpy)
+    image_pil.save(filename)
+    print("Image saved as {}".format(filename))
+    
+    
+    
+    
+    
+class CGAN_DatasetFromFolder(data.Dataset):
+    def __init__(self, image_dir, direction):
+        super(DatasetFromFolder, self).__init__()
+        self.direction = direction
+        self.a_path = join(image_dir, "a")
+        self.b_path = join(image_dir, "b")
+        self.image_filenames = [x for x in listdir(self.a_path) if is_image_file(x)]
+
+        transform_list = [transforms.ToTensor(),
+                          transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+
+        self.transform = transforms.Compose(transform_list)
+
+    def __getitem__(self, index):
+        a = Image.open(join(self.a_path, self.image_filenames[index])).convert('RGB')
+        b = Image.open(join(self.b_path, self.image_filenames[index])).convert('RGB')
+        a = a.resize((286, 286), Image.BICUBIC)
+        b = b.resize((286, 286), Image.BICUBIC)
+        a = transforms.ToTensor()(a)
+        b = transforms.ToTensor()(b)
+        w_offset = random.randint(0, max(0, 286 - 256 - 1))
+        h_offset = random.randint(0, max(0, 286 - 256 - 1))
+    
+        a = a[:, h_offset:h_offset + 256, w_offset:w_offset + 256]
+        b = b[:, h_offset:h_offset + 256, w_offset:w_offset + 256]
+    
+        a = transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))(a)
+        b = transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))(b)
+
+        if random.random() < 0.5:
+            idx = [i for i in range(a.size(2) - 1, -1, -1)]
+            idx = torch.LongTensor(idx)
+            a = a.index_select(2, idx)
+            b = b.index_select(2, idx)
+
+        if self.direction == "a2b":
+            return a, b
+        else:
+            return b, a
+
+    def __len__(self):
+        return len(self.image_filenames)
+    
+
+
+    
+    
+    
+
