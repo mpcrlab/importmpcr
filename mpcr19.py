1a9cf7a300ea2f28de377938161b2c8a173c98e6
William E Hahn <williamedwardhahn@gmail.com>
2019-03-03 21:50:31 -0500

    Update mpcr19.py

--- mpcr19.py
+++ mpcr19.py
@@ -167,7 +167,7 @@ def imshow(inp, title=None):
     
     
     
-def train_model(model, criterion, optimizer, scheduler, num_epochs=25, dataloaders,class_names,dataset_sizes,device):
+def train_model(dataloaders,class_names,dataset_sizes,device, model, criterion, optimizer, scheduler, num_epochs=25):
     since = time.time()
 
     best_model_wts = copy.deepcopy(model.state_dict())
