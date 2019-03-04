5b8513101db16724222076064fcdafa085887dce
William E Hahn <williamedwardhahn@gmail.com>
2019-03-03 20:39:43 -0500

    Update mpcr19.py

--- mpcr19.py
+++ mpcr19.py
@@ -1,1 +1,17 @@
-!pip install google_images_download
+name='google_images_download'
+
+import subprocess
+
+def install(name):
+    subprocess.call(['pip', 'install', name])
+
+install(name)
+
+
+from google_images_download import google_images_download
+import shutil
+import os
+import cv2
+
+
+
