6e689b455deb9277f39e2e276e6942778e2416c4
mishakmak <mishakmak@gmail.com>
2019-04-12 14:05:55 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-your-username",
+    name="ImportMPCR",
     version="0.0.1",
-    author="Example Author",
-    author_email="author@example.com",
-    description="A small example package",
+    author="MPCR Lab",
+    author_email="mpcrlab@gmail.com",
+    description="Import everything used in the MPCR lab.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/mpcrlab/importmpcr",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
