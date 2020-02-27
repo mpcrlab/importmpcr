3ba3595d74bc7102071817c2162b3a5339b3e81a
mishakmak <mishakmak@gmail.com>
2020-02-27 15:44:11 -0500

    Update inlineops.py

--- mpcr/inlineops.py
+++ mpcr/inlineops.py
@@ -1,13 +1,13 @@
 from .infix import *
 
-@make_infix('mod','pow','rshift','mul')
+@make_infix('mod','pow','rshift','mul','div','fdiv')
 def to(start,end):
     return list(range(start,end))
 
-@make_infix('mod','pow','rshift','mul')
+@make_infix('mod','pow','rshift','mul','div','fdiv')
 def take(gen,count):
     return [next(gen) for _ in range(count)]
 
-@make_infix('mod','pow','rshift','mul')
+@make_infix('mod','pow','rshift','mul','div','fdiv')
 def of(func,list):
     return [func(i) for i in list]
