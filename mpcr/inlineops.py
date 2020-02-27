4f5362be0d2123a27ca6732cdd9d74a8f347f0c0
mishakmak <mishakmak@gmail.com>
2020-02-27 15:02:48 -0500

    Add sample inline ops

---
+++ mpcr/inlineops.py
@@ -0,0 +1,13 @@
+from .infix import *
+
+@make_infix('mod','pow','rshift','mul')
+def to(start,end):
+    return list(range(start,end))
+
+@make_infix('mod','pow','rshift','mul')
+def take(gen,count):
+    return [next(gen) for _ in range(count)]
+
+@make_infix('mod','pow','rshift','mul')
+def to(start,end):
+    return list(range(start,end))
